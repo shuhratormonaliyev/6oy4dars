7d05a01e068ecaad0f4eb11fef99675be20f7745
shuhratormonaliyev <shuratormonaliyev7@gmail.com>
2024-07-21 16:20:55 +0500

    problems

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import Header from './Header';
+import Form from './Form';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Form />
+    </div>
+  );
+};
+
+export default App;
